0b4d64d0005bb18c09328aadb2b2c78f7f9fcea8
meir <m.stigel@gmail.com>
2020-12-28 14:02:22 +0200

    first upload

---
+++ game_back.js
@@ -0,0 +1,24 @@
+console.log('runs!!!')
+const http = require('http');
+const app = require('express')();
+const server = http.createServer(app);
+const io = require('socket.io')(server);
+
+
+app.get('/',(req, res) => {
+    res.sendFile(__dirname + '\\gest.html');
+})
+
+io.sockets.on('connection', socket => {
+    socket.on('start', num => {
+        io.sockets.emit('start', num);
+    })
+    socket.on('go', num => {
+        io.sockets.emit('go', num);
+    })
+    socket.on('send_gest', gest => {
+        io.sockets.emit('get_gest', gest)
+    })
+})
+
+server.listen(1000);
